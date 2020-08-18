583fd479782dca306ad1efcefb2d07e7b9d36107
melnik <kivals.90@yandex.ru>
2020-08-18 15:50:59 +0300

    Add Logo-uploader

--- src/pages/AdminMainInfo.vue
+++ src/pages/AdminMainInfo.vue
@@ -1,11 +1,45 @@
 <template>
-  <h1>AdminMainInfo</h1>
+  <div>
+    <ul>
+      <li>
+        <p>Поменять лого</p>
+        <input type="file" @change="onFileChange" />
+        <div class="preview">
+          <img v-if="url" :src="url" />
+        </div>
+      </li>
+      <li>
+        <p>Редактировать меню</p>
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AdminMainInfo',
+  data() {
+    return {
+      url: null,
+    };
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.preview {
+  img {
+    width: 150px;
+  }
+}
+li  {
+  margin-top: 20px;
+}
+</style>
