d23cf604dc3ed9b5a7fa759fec61078f13e2bb20
melnik <kivals.90@yandex.ru>
2020-08-19 15:44:31 +0300

    Add validate for Login component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vuelidate from 'vuelidate/src';
 
 import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
+
+/* Plugins */
+Vue.use(Vuelidate);
 
 new Vue({
   router,
