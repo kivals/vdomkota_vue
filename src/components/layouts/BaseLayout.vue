55a06c3a2e5c16ea2f03673eb591c1cf0e8bd50a
melnik <kivals.90@yandex.ru>
2020-08-27 16:02:50 +0300

    Turn on Footer section

--- src/components/layouts/BaseLayout.vue
+++ src/components/layouts/BaseLayout.vue
@@ -2,16 +2,19 @@
   <div>
     <app-header></app-header>
     <router-view />
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/layouts/header/AppHeader';
+import AppFooter from '@/components/layout/AppFooter';
 
 export default {
   name: 'BaseLayout',
   components: {
     AppHeader,
+    AppFooter,
   },
 };
 </script>
